f9d668c3f9931ad1b2ed055399da6d677c987988
Vitaliy <movcan77@gmail.com>
2026-05-16 13:55:42 +0300

    Fix problems, animate crystal

--- main.py
+++ main.py
@@ -33,19 +33,36 @@ class CoffeeManager(QWidget):
         self.exp = 0
         self.level = 1
         self.exp_to_next = 20
-
+        self.upgrade_click_price = 5
+        self.upgrade_auto_price = 10
         self.achievements = set()
         self.crystal_active = False
         self.init_ui()
 
-        self.upgrade_click_price = 5
-        self.upgrade_auto_price = 10
+        self.crystal_dx = 4
+        self.crystal_dy = 4
+        self.crystal_move_timer = QTimer()
+        self.crystal_move_timer.timeout.connect(self.move_crystal)
+
+        self.crystal_timer = QTimer()
+        self.crystal_timer.timeout.connect(self.spawn_crystal)
+        self.crystal_timer.start(3000)
 
         self.timer = QTimer()
         self.timer.timeout.connect(self.game_loop)
         self.timer.start(1000)
 
     def init_ui(self):
+        self.setStyleSheet("""
+            QWidget { background-color: #1e1e2f; color: white; font-size: 15px }
+            QPushButton {
+                background-color: #3a3a5f;
+                border-radius: 12px;
+                padding: 10px;
+                font-size: 15px;
+            }
+            QPushButton:hover { background-color: #50507a; }
+        """)
         self.setWindowTitle("Coffee Manager Clicker")
         self.setFixedSize(400, 500)
 
@@ -65,14 +82,22 @@ class CoffeeManager(QWidget):
         self.upgrade_click_btn.clicked.connect(self.upgrade_click)
 
         self.label_click_price = QLabel(self)
-        # self.label_click_price.setText(str(self.upgrade_click_price))
-        self.label_click_price.setGeometry(155, 315, 100, 30)
+        self.label_click_price.setText(str(self.upgrade_click_price))
+        self.label_click_price.setGeometry(155, 220, 100, 30)
+        self.label_click_price.setAlignment(Qt.AlignCenter)
+
+        self.label_auto_price = QLabel(self)
+        self.label_auto_price.setText(str(self.upgrade_auto_price))
+        self.label_auto_price.setGeometry(260, 220, 100, 30)
+        self.label_auto_price.setAlignment(Qt.AlignCenter)
 
         self.upgrade_auto_btn = QPushButton("🤵🏻 auto +1", self)
         self.upgrade_auto_btn.setGeometry(260, 150, 100, 60)
+        self.upgrade_auto_btn.clicked.connect(self.upgrade_auto)
 
         self.crystal_btn = QPushButton("💎", self)
         self.crystal_btn.setGeometry(50, 360, 50, 50)
+        self.crystal_btn.hide()
         self.update_ui()
 
     def update_ui(self):
@@ -81,6 +106,8 @@ class CoffeeManager(QWidget):
         )
         self.progress.setMaximum(self.exp_to_next)
         self.progress.setValue(self.exp)
+        self.label_click_price.setText(str(self.upgrade_click_price))
+        self.label_auto_price.setText(str(self.upgrade_auto_price))
 
     def sell_coffee(self):
         self.money += self.income_per_click
@@ -106,10 +133,62 @@ class CoffeeManager(QWidget):
 
     def upgrade_click(self):
         if self.money >= self.upgrade_click_price:
-            self.money -= self.upgrade_click_btn
+            self.money -= self.upgrade_click_price
             self.income_per_click += 1
             self.upgrade_click_price *= 1.3
             self.upgrade_click_price = round(self.upgrade_click_price)
+            self.update_ui()
+
+    def upgrade_auto(self):
+        if self.money >= self.upgrade_auto_price:
+            self.money -= self.upgrade_auto_price
+            self.auto_income += 1
+            self.upgrade_auto_price *= 1.3
+            self.upgrade_auto_price = round(self.upgrade_click_price)
+            self.update_ui()
+
+    def hide_crystal(self):
+        self.crystal_btn.hide()
+        self.crystal_active = False
+
+    def spawn_crystal(self):
+        if not self.crystal_active and random.randint(1, 5) == 1:
+            self.crystal_active = True
+
+            x = random.randint(50, 350)
+            y = random.randint(220, 450)
+            self.crystal_btn.move(x, y)
+            self.crystal_btn.show()
+
+            self.crystal_dx = random.choice([-5, -4, 4, 5])
+            self.crystal_dy = random.choice([-5, -4, 4, 5])
+            self.crystal_move_timer.start(16)
+            QTimer.singleShot(10000, self.hide_crystal)
+
+    def move_crystal(self):
+        if not self.crystal_active:
+            return
+        x = self.crystal_btn.x()
+        y = self.crystal_btn.y()
+        new_x = x + self.crystal_dx
+        new_y = y + self.crystal_dy
+
+        left = 0
+        right = self.width() - self.crystal_btn.width()
+
+        top = 220
+        bottom = self.height() - self.crystal_btn.height()
+
+        if new_x <= left or new_x >= right:
+            self.crystal_dx *= -1
+        if new_y <= top or new_y >= bottom:
+            self.crystal_dy *= -1
+
+        self.crystal_dx = max(-10, min(10, self.crystal_dx))
+        self.crystal_dy = max(-10, min(10, self.crystal_dy))
+
+        self.crystal_btn.move(x + self.crystal_dx, y + self.crystal_dy)
+
 
 app = QApplication(sys.argv)
 window = CoffeeManager()
